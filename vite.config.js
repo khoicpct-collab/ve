ea2432924c16691780b8abd0febf69ac54f70a12
khoicpct-collab <khoicpct@gmail.com>
2025-12-02 13:16:44 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⚠️ THAY flow-simulator-full bằng đúng tên repo của bạn
 export default defineConfig({
   plugins: [react()],
-  base: '/flow-simulator-full/'
+  base: '/ve/'
 })
